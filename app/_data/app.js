d68d1518c089716d32a2667ccf803f41c135d13b
Roel Antonisse <r.antonisse@accessibility.nl>
2020-07-24 22:29:17 +0200

    Add basic app global data, and set build Date there

---
+++ app/_data/app.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: '{{ pkg.name }}',
+  title: 'WCAG Report Tool',
+  buildDate: new Date(),
+  locales: [
+    {
+      code: 'en',
+      label: 'English',
+      default: true
+    },
+    {
+      code: 'nl',
+      label: 'Nederlands'
+    }
+  ]
+};
